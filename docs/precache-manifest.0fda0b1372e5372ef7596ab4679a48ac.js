fd46c0111eb500c4df13ce5fa09d6d3287f4204b
mgarvia <maria.garvia.soler@gmail.com>
2020-04-25 13:02:22 +0200

    docs

---
+++ docs/precache-manifest.0fda0b1372e5372ef7596ab4679a48ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8edb946d67310b728b44abef8f7569",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a169501264b5cbc1ac39",
+    "url": "./static/css/main.5b8dd591.chunk.css"
+  },
+  {
+    "revision": "1176af410a47f27cde52",
+    "url": "./static/js/2.34103eef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.34103eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a169501264b5cbc1ac39",
+    "url": "./static/js/main.95d09adf.chunk.js"
+  },
+  {
+    "revision": "41c2f3c96e512543836a",
+    "url": "./static/js/runtime-main.8bab44d2.js"
+  },
+  {
+    "revision": "842f63c30ea08a7817170cad48d5ebfb",
+    "url": "./static/media/loader-img.842f63c3.png"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
